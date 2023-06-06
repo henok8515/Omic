d64778d703f17b1d5d5e158e20898c6837e69023
henok8515 <henokegezew44@gmail.com>
2023-06-06 15:59:23 +0300

    adding folder tree

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -3,8 +3,8 @@ import { ArrowDropDown, ArrowDropDownCircleOutlined } from "@material-ui/icons";
 // import CommitIcon from "@mui/icons-material/Commit";
 function Header() {
   return (
-    <div className=" flex justify-between bg-gray-800  h-16 text-white  items-center">
-      <div className="flex items-center justify-around h-full w-5/12 ml-3 ">
+    <div className=" flex justify-between bg-gray-800  h-12  text-xs h-16 text-white  items-center">
+      <div className="flex items-center  h-full w-5/12 ml-3 ">
         <div className="flex items-center">
           <Typography variant="p " className="">
             Project Name
@@ -13,15 +13,15 @@ function Header() {
             <ArrowDropDownCircleOutlined />
           </span>
         </div>
-        <button className=" text-sm flex items-center justify-between  font-semibold text-white py-1 px-6 border border-white  rounded">
+        <button className=" text-xs flex items-center justify-between  ml-2 mr-2 font-semibold text-white py-1 px-3 border border-white  rounded">
           Development
           <ArrowDropDown />
         </button>
         <Button
           style={{
             backgroundColor: "skyblue",
-            padding: "7px 20px",
-            fontSize: "small",
+            padding: "7px 10px",
+            fontSize: "10px",
             textTransform: "capitalize",
           }}
         >
