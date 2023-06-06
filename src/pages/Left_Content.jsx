38c1c3f33ca8a327a4f2c65a41b75b161a4772e5
henok8515 <henokegezew44@gmail.com>
2023-06-06 20:37:44 +0300

    feat: finalizing the project

--- src/pages/Left_Content.jsx
+++ src/pages/Left_Content.jsx
@@ -3,30 +3,30 @@ import FolderStructure from "../components/FolderStructure";
 
 function Left_Content() {
   return (
-    <div className="p-5 flex flex-col w-1/5 text-xs  bg-gray-600 max-h-screen">
-      <Typography variant="h5" className="p-3 font-extrabold text-white ">
+    <div className="p-5 flex flex-col w-1/6 text-xs  bg-gray-600 max-h-screen">
+      <Typography variant="h4" className=" font-extrabold text-white ">
         Files
       </Typography>
-      <div className="flex text-start flex-col text-white font-bold">
-        <div className="flex m-2 items-center">
+      <div className="flex text-start   flex-col text-white ">
+        <div className=" w-full flex m-2 items-center">
+          <span className="rounded-full mt-0 mr-2 py-1 px-5 text-white bg-teal-500">
+            config
+          </span>{" "}
+          <Typography variant="p">dataform</Typography>
+        </div>
+        <div className="flex  w-full m-2 items-center">
           <span className="rounded-full mr-2 py-1 px-5 text-white bg-teal-500">
             config
           </span>{" "}
-          <Typography>dataform</Typography>
+          <Typography variant="p">package-lock</Typography>
         </div>
-        <div className="flex m-2 items-center">
+        <div className="flex w-full m-2 items-center">
           <span className="rounded-full mr-2 py-1 px-5 text-white bg-teal-500">
             config
           </span>{" "}
-          <Typography>dataform</Typography>
+          <Typography variant="p">package</Typography>
         </div>
-        <div className="flex m-2 items-center">
-          <span className="rounded-full mr-2 py-1 px-5 text-white bg-teal-500">
-            config
-          </span>{" "}
-          <Typography>dataform</Typography>
-        </div>
-        <div className="flex  ml-6">
+        <div className="flex mt-5 ml-2">
           <FolderStructure />
         </div>
       </div>
